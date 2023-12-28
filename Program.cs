4f00f2898d276e2f5b1aad21d9a05f4726d27fed
Danilo Leal <dcpleal@gmail.com>
2023-12-28 10:43:53 -0300

    trilha-net-poo-desafio
    Finalizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,44 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+string nrocelular;
+string modelcelular;
+string imeicelular;
+int qtdememoria;
+string app;
+
+Console.WriteLine("Smartphone Nokia:");
+
+Console.WriteLine("Qual o número do celular:");
+nrocelular = Console.ReadLine();
+Console.WriteLine("Qual o modelo do celular:");
+modelcelular = Console.ReadLine();
+Console.WriteLine("Qual o IMEI do celular:");
+imeicelular = Console.ReadLine();
+Console.WriteLine("Qual a qtde de memória do celular:");
+qtdememoria = int.Parse(Console.ReadLine());
+Console.WriteLine("Qual o aplicativo que deseja instalar no celular:");
+app = Console.ReadLine();
+
+
+Smartphone nokia = new Nokia(numero: nrocelular, modelo: modelcelular, imei: imeicelular, memoria: qtdememoria);
+nokia.Ligar();
+nokia.InstalarAplicativo(app);
+
+Console.WriteLine("");
+Console.WriteLine("Smartphone Iphone:");
+
+Console.WriteLine("Qual o número do celular:");
+nrocelular = Console.ReadLine();
+Console.WriteLine("Qual o modelo do celular:");
+modelcelular = Console.ReadLine();
+Console.WriteLine("Qual o IMEI do celular:");
+imeicelular = Console.ReadLine();
+Console.WriteLine("Qual a qtde de memória do celular:");
+qtdememoria = int.Parse(Console.ReadLine());
+Console.WriteLine("Qual o aplicativo que deseja instalar no celular:");
+app = Console.ReadLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: nrocelular, modelo: modelcelular, imei: imeicelular, memoria: qtdememoria);
+iphone.Ligar();
+iphone.InstalarAplicativo(app);
